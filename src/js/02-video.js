57474fdfe3b04e6b7fe6872713802d8a89fc57ad
Taya <taiska1488@gmail.com>
2023-01-20 22:17:53 +0200

    Summery video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframeVideoplayer = document.querySelector('#vimeo-player');
+const player = new Player(iframeVideoplayer);
+const CURRENT_TIME = 'videoplayer-current-time';
+
+if (localStorage.getItem(CURRENT_TIME)) {
+    player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
+}
+
+player.on('timeupdate', throttle(onUpdateTime, 1000));
+
+function onUpdateTime(data) {
+    localStorage.setItem(CURRENT_TIME, JSON.stringify(data.seconds));
+}
